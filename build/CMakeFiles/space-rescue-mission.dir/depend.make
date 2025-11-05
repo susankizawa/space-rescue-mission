09ab49874fcd3fe7209f1fb98e9f1b007d0b4363
Susan K. Izawa <susan.k.izawa@gmail.com>
2025-11-05 04:12:16 -0300

    fix: applied gitignore

--- build/CMakeFiles/space-rescue-mission.dir/depend.make
+++
@@ -1,2 +0,0 @@
-# Empty dependencies file for space-rescue-mission.
-# This may be replaced when dependencies are built.
--- build/external/ftxui/CMakeFiles/dom.dir/src/ftxui/dom/flexbox_config.cpp.obj.d
+++
@@ -1,3 +0,0 @@
-external/ftxui/CMakeFiles/dom.dir/src/ftxui/dom/flexbox_config.cpp.obj: \
- D:\Projects\C++\space-rescue-mission\external\ftxui\src\ftxui\dom\flexbox_config.cpp \
- D:/Projects/C++/space-rescue-mission/external/ftxui/include/ftxui/dom/flexbox_config.hpp
